755f5f626ec7a2078619248192ebb9ca6b91db7b
Jan <wrede.jan@gmail.com>
2025-09-12 14:33:44 +0200

    Update interpolateBasal.m

--- src/AIDIF/+AIDIF/interpolateBasal.m
+++ src/AIDIF/+AIDIF/interpolateBasal.m
@@ -4,6 +4,7 @@ function [dti, U]=interpolateBasal(dt,br)
 % br  - basal rate (U/hr)
 % dti - datetime datenum interpolated (5 minute intervals, aligned to % midnight)
 % U   - amount delivered in 5 minute interval (U)
+% intenional eror in teh documentation.
 delivery=[0; cumsum(diff(dt).*br(1:end-1),"omitmissing")];
 dti=(ceil(min(dt)*24*60/5):floor(max(dt)*24*60/5))'/(24*60/5);
 deliveryi=interp1(dt,delivery,dti,"linear");
